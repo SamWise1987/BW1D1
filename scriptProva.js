eccc92376ee0fffb9d0e2133223c151e6f5bed73
SamWise1987 <samuelerea23@gmail.com>
2024-01-17 20:20:54 +0100

    Aggiornamenti JS e html

--- scriptProva.js
+++ scriptProva.js
@@ -2,34 +2,67 @@ let timer = 60;
 let score = 0;
 let countQuestion = 0;
 
-const countdown = setInterval(() => {
-    timer--;
-    if (timer === 0) {
-        clearInterval(countdown);
+// seleziono l'elemento HTML che mostra i numeri del timer
+let timerDigitElement = document.getElementById('timerDigit');
+// imposto il timer iniziale a 30 secondi
+let timerCount = 30;
+
+// starto il display del timer con il valore iniziale (30 secondi)
+timerDigitElement.textContent = timerCount;
+
+// creo l'intervallo che si attiva ogni secondo
+let interval = setInterval(function () {
+    // decremento il timer di 1 ogni secondo
+    timerCount--;
+
+    // aggiorno il display del timer
+    timerDigitElement.textContent = timerCount;
+
+    // se il timer arriva a 0, lo ferma e non lo fa ripartire
+    if (timerCount === 0) {
+        clearInterval(interval); // fermo l'intervallo
+        timerDigitElement.textContent = "Fine!"; // cambio il testo a 'Fine!'
+    }
+}, 1000); // imposto l'intervallo di tempo a 1000 millisecondi (1 secondo)
+
+//const countdown = setInterval(() => {
+//  timer--;
+//if (timer === 0) {
+//  clearInterval(countdown);
+//}
+
+//}, 1000)
+
+
+function displayQuestion() {
+    if (countQuestion < questions.lenght) {
+        let currentQuestion = questions.question[countQuestion];
+
+        let questionDiv = document.createElement("div");
+        questionDiv.classList.add("questionDiv");
+        questionDiv.textContent = currentQuestion.question;
+        document.body.appendChild(questionDiv);
+
+        countQuestion++;
     }
 
-}, 1000)
+};
 
+function callAnswer (managingAnswers){
 
-function displayQuestion(){
-    let question = document.querySelector(".questionDiv");
-    let answers = document.querySelectorAll(".radioBtn");
+    let currentAnswer = [currentAnswer.correct_answer, ...currentAnswer.inceorrect_answers];
     
+    shuffle(answers)
 
+    for(i=0; i < answers.lenght; i++){
+        let answerDiv = document.createElement("div");
+        answerDiv.classList.add("answerBox");
+        answerDiv.textContent =answers[i];
+        document.body.appendChild(answerDiv);
+
+    }
 }
 
-
-per ogni elemento dell'array visualiizzami l'indice zer, crea il bottone delle risposte, in base a quella che clicco ti prenderai il testo che è all'interno di correct_answer.
-
-//function displayQuestion() {
-  //  if (countQuestion < quastions.lengh) {
-    //    let question = questions[countQuestion];
-      //  let answers = [questions.correct_answer, ...questions.incorrect_answers];
-      //  shuffle(answers);
-
-   // }
-
-// };
 
 function correctAnswer() {
     if (selectedAnswer === questions.correct_answer) {
@@ -37,12 +70,13 @@ function correctAnswer() {
     }
 
     countQuestion++;
+    displayQuestion();
 }
 
 
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+function shuffle(answers) {
+    for (let i = answers.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        [answers[i], answers[j]] = [answers[j], answers[i]];
     }
 }
